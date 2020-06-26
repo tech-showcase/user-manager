687643aa76decea8e9d6c6c99942d3c421e9bb31
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-06-26 23:43:13 +0700

    feat(users): update user model
    - secure password field
    - add phone field validation

--- users/models.py
+++ users/models.py
@@ -1,13 +1,25 @@
 from django.db import models
-from django import forms
-from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import RegexValidator
+from django.contrib.auth.hashers import make_password
+
+
+class PasswordField(models.CharField):
+    description = "A field for inputting password securely."
+
+    def pre_save(self, instance, add):
+        val = make_password(instance)
+        setattr(instance, self.attname, val)
+        return val
 
 
 class User(models.Model):
     username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50, widget=forms.PasswordInput)
+    password = PasswordField(max_length=50)
     email = models.EmailField(max_length=75)
-    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    phone_regex_validator = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                           message="Phone number must be entered in the format: '+999999999'."
+                                                   "Up to 15 digits allowed.")
+    phone = models.CharField(validators=[phone_regex_validator], max_length=15)
     first_name = models.CharField(max_length=75)
     last_name = models.CharField(max_length=75)
     address = models.CharField(max_length=150)
