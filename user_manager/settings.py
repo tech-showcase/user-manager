10a6fe596eac756f50830e4773161b5ebf2556ab
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-11 20:11:58 +0700

    refactor(users): rename user config to users config

--- user_manager/settings.py
+++ user_manager/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UserConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
