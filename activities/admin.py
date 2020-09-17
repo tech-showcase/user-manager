e06507c2fcff575eda3229a92fefb112515fc5ff
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-09-18 02:41:35 +0700

    feat(activities): add api access model and its admin page

--- activities/admin.py
+++ activities/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import APIAccess
 
-# Register your models here.
+admin.site.register(APIAccess)
--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class APIAccess(models.Model):
+    api_name = models.CharField(max_length=50)
+    status_code = models.PositiveSmallIntegerField()
+    response_time = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('users.User', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"{self.api_name}-{self.created_at}"
