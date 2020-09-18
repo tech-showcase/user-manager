5562028c370d486a3c7e89cc19b9645c3ecb40ac
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-09-18 15:30:43 +0700

    feat(activities): create migration file for api access
    - create special init migration file for partitioned table

--- activities/models.py
+++ activities/models.py
@@ -5,12 +5,13 @@ class APIAccess(models.Model):
     api_name = models.CharField(max_length=50)
     status_code = models.PositiveSmallIntegerField()
     response_time = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(primary_key=True, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey('users.User', on_delete=models.DO_NOTHING)
 
     class Meta:
         ordering = ['created_at']
+        managed = False
 
     def __str__(self):
         return f"{self.api_name}-{self.created_at}"
