4420a3bf4a37792f45d14447cfb4faff78f5647e
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-08 11:26:23 +0700

    feat(user_manager): add rest framework setting

--- user_manager/settings.py
+++ user_manager/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'activities.apps.ActivitiesConfig'
+    'activities.apps.ActivitiesConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
