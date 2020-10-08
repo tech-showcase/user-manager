aca1230c298dcc7487de903d905df6b3e71db145
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-10-08 11:28:12 +0700

    feat(user_manager): add api auth and users api to url pattern

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('users.urls'))
 ]
